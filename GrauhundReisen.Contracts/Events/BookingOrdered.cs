0e355c4698de590ecdf379f8d40cd76fd130d2ec
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 11:41:52 +0200

    Adding event properties

--- GrauhundReisen.Contracts/Events/BookingOrdered.cs
+++ GrauhundReisen.Contracts/Events/BookingOrdered.cs
@@ -2,6 +2,11 @@
 {
   public class BookingOrdered
   {
-     
+    public string BookingId { get; set; }
+    public string Destination { get; set; }
+    public string CreditCardNumber { get; set; }
+    public string CreditCardType { get; set; }
+    public string Email { get; set; }
+    public string LastName { get; set; }
   }
 }
