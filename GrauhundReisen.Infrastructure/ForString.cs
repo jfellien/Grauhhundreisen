232ee6e437bc5924dabbe30942eab81872071093
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 12:01:15 +0200

    Adding configuration class for EventStoreClient

---
+++ GrauhundReisen.Infrastructure/ForString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrauhundReisen.Infrastructure
+{
+  public static class ForString
+  {
+    public static bool IsNotNullOrEmpty(this string value)
+    {
+      return !String.IsNullOrWhiteSpace(value);
+    }
+  }
+}
