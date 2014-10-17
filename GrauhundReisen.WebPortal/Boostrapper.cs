04723bad717564fab9d96271b157fc51a6b8d096
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 10:45:41 +0200

    Reintending code

--- GrauhundReisen.WebPortal/Boostrapper.cs
+++ GrauhundReisen.WebPortal/Boostrapper.cs
@@ -3,13 +3,13 @@ using Nancy.TinyIoc;
 
 namespace GrauhundReisen.WebPortal
 {
-    public class Boostrapper : DefaultNancyBootstrapper
+  public class Boostrapper : DefaultNancyBootstrapper
+  {
+    protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
     {
-        protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
-        {
-            StaticConfiguration.DisableErrorTraces = false;
+      StaticConfiguration.DisableErrorTraces = false;
 
-            base.ApplicationStartup(container, pipelines);
-        }
+      base.ApplicationStartup(container, pipelines);
     }
+  }
 }
