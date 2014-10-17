e7e168f719c8f6a3fb06c1b6702d6e9541ac4fb4
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 12:00:57 +0200

    Adding Extension checking null or empty of a string

---
+++ GrauhundReisen.Infrastructure/EventStoreClientConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GrauhundReisen.Infrastructure
+{
+  public class EventStoreClientConfiguration
+  {
+    public Uri ServerUri { get; set; }
+    public string InitActionName { get; set; }
+    public string StoreActionName { get; set; }
+    public string RetrieveActionName { get; set; }
+
+    public string RemoveActionName { get; set; }
+
+    public string AccountId { get; set; }
+
+    public bool IsComplete()
+    {
+      return ServerUri.ToString().IsNotNullOrEmpty()
+             && InitActionName.IsNotNullOrEmpty()
+             && StoreActionName.IsNotNullOrEmpty()
+             && RetrieveActionName.IsNotNullOrEmpty()
+             && AccountId.IsNotNullOrEmpty();
+    }
+  }
+}
