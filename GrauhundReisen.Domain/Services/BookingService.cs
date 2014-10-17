8865e6c7fc86de08cfd3799bb88efde84bbdb2e6
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 11:32:07 +0200

    Implementing booking of a new order

--- GrauhundReisen.Domain/Services/BookingService.cs
+++ GrauhundReisen.Domain/Services/BookingService.cs
@@ -1,7 +1,27 @@
-﻿namespace GrauhundReisen.Domain.Services
+﻿using System.Threading.Tasks;
+
+namespace GrauhundReisen.Domain.Services
 {
   public class BookingService
   {
-     
+    public async Task Orderbooking(string bookingId,
+      string destination,
+      string creditCardNumber, string creditCardType,
+      string email, string firstName, string lastName)
+    {
+      var booking = new Booking();
+
+      booking.Order(bookingId,
+        destination,
+        creditCardNumber, creditCardType,
+        email, firstName, lastName);
+
+      await Task.Factory.StartNew(() => booking.Changes.ToList().ForEach(
+        change =>
+        {
+          _eventStoreClient.Store(bookingId, change);
+          _eventHandling(change);
+        }));
+    }
   }
 }
