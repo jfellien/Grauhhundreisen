642c2af21f967bc3a4a2fd120c40a282d4c6f488
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-10-17 11:51:18 +0200

    Adding EventBag

---
+++ GrauhundReisen.Infrastructure/EventBag.cs
@@ -0,0 +1,11 @@
+﻿namespace GrauhundReisen.Infrastructure
+{
+  public class EventBag
+  {
+    public string Type { get; set; }
+    public string EventData { get; set; }
+    public string TimeStamp { get; set; }
+    public string EntityId { get; set; }
+    public string EventId { get; set; }
+  }
+}
